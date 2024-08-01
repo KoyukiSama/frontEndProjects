4f435bb196093abc41f1fe7031dc7ebb3930bcc5
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-08-01 22:26:49 +0200

    I finished the full parser and translator functions, now you can type anything and it will parse it and then translate it. I'm quite happy with how it turned out, especially because I didn't use any tutorials or anything like that.

--- projects/zsmall-projects/morsecodeTranslator/script.js
+++ projects/zsmall-projects/morsecodeTranslator/script.js
@@ -21,7 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
 let usrInputElement = document.getElementById("usrInputElement");
 let usrOutputElement = document.getElementById("usrOutputElement");
 
-    
+
+
+//      PARSING PART
+//
   
 // splitter for morse or string or unknown
 // adds 'm' 's' 'u' to the start and puts them into arrays.
@@ -54,7 +57,7 @@ function splitToArray(text) {
         // if morse char
         else if (text[i].isMorse()) {
             splittedArray[x] = ['m'];
-            let y = 0;
+            let y = 1;
             while (i < text.length && text[i].isMorse()) {
                 // add all morse into string blocks
                 if (text[i].isMorseNoBs()) {
@@ -104,7 +107,13 @@ function splitToArray(text) {
     return splittedArray;
 }
 
-// text to morsecode
+
+
+//      TRANSLATION PART
+//
+
+
+// sub translator: text to morsecode
 function textToMorse(text) {
     let morsecodeDict = {
         'E': ".", 'T': "-", 'A': ".-", 'O': "---",
@@ -115,29 +124,115 @@ function textToMorse(text) {
         'V': "...-", 'K': "-.-", 'J': ".---", 'X': "-..-",
         'Q': "--.-", 'Z': "--..", '1': ".----", '2': "..---",
         '3': "...--", '4': "....-", '5': ".....", '6': "-....",
-        '7': "--...", '8': "---..", '9': "----.", '0': "-----",
-        ' ': "/",
+        '7': "--...", '8': "---..", '9': "----.", '0': "-----"
     };
   
     let morseCode = [];
-    for (let i = 0, j = 0; i < text.length; i++, j += 2) {
-    morseCode[j] = morsecodeDict[text[i]];
-        if (text[i + 1] != null) {
-            morseCode[j + 1] = ' ';
-        };
+    text = text.toUpperCase();
+    for (let i = 0; i < text.length; i++) {
+        morseCode[i] = morsecodeDict[text[i]];
     };
 
-    return morseCode = morseCode.join('');
+    return morseCode.join(' ');
 };
+
+// sub translator: morsecode to text
+function morseToText(morse) {
+    let textcodeDict = {
+        ".": 'E', "-": 'T', ".-": 'A', "---": 'O',
+        "..": 'I', "-.": 'N', "...": 'S', "....": 'H',
+        ".-.": 'R', "-..": 'D', ".-..": 'L', "-.-.": 'C',
+        "..-": 'U', "--": 'M', ".--": 'W', "..-.": 'F',
+        "--.": 'G', "-.--": 'Y', ".--.": 'P', "-...": 'B',
+        "...-": 'V', "-.-": 'K', ".---": 'J', "-..-": 'X',
+        "--.-": 'Q', "--..": 'Z', ".----": '1', "..---": '2',
+        "...--": '3', "....-": '4', ".....": '5', "-....": '6',
+        "--...": '7', "---..": '8', "----.": '9', "-----": '0'
+    };
+  
+    let text = [];
+
+    return text = textcodeDict[morse];
+};
+
+
+// main translator function
+
+function textMorseTrans(splittedArray) {
+    
+    let translatedArray = [];
+
+    let i = 0, x = 0;
+    while (x < splittedArray.length) {
+        
+        // checks if string
+        if (splittedArray[x][0] == 's') {
+
+            let y = 1;
+            while (y < splittedArray[x].length) {
+                translatedArray[i] = textToMorse(splittedArray[x][y]);
+                i++;
+                y++;
+
+                if (y < splittedArray[x].length) {
+                    translatedArray[i] = '/';
+                    i++;
+                }
+            }
+        }
+
+        // checks if morse
+        if (splittedArray[x][0] == 'm') {
+
+            let dummyArray = [];
+            let a = 0;
+            let y = 1;
+            while (y < splittedArray[x].length) {
+                if (splittedArray[x][y].isMorseNoBs()) {
+                    dummyArray[a] = morseToText(splittedArray[x][y]);
+                    a++;
+                    y++;
+                }
+                if (splittedArray[x][y] == '/') {
+                    dummyArray[a] = ' ';
+                    a++;
+                    y++;
+                }
+            }
+            translatedArray[i] = dummyArray.join('') // join with no spaces
+            i++;
+        }
+
+        // checks if unknown
+        if (splittedArray[x][0] == 'u') {
+
+            let dummyArray = [];
+            let a = 0;
+            y = 1;
+            while (y < splittedArray[x].length) {
+                dummyArray[a] = splittedArray[x][y];
+                a++;
+                y++;
+            }
+            translatedArray[i] = dummyArray.join('') // join with no spaces
+            i++;
+        }
+        x++; // go to next type (s, m, u) array
+    }
+    
+    return translatedArray.join(' ');
+}
     
 
 // Event listener
 usrInputElement.addEventListener("input", function() {
     let usrInput = usrInputElement.value;
     let splittedArray = splitToArray(usrInput);
+    let translatedTextMorseUnknown = textMorseTrans(splittedArray);
     console.log(splittedArray);
+    console.log(translatedTextMorseUnknown);
         
-    // usrOutputElement.textContent = morseCode;
+    usrOutputElement.textContent = translatedTextMorseUnknown;
 });
     
 });
